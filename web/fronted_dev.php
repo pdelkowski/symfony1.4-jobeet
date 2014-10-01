89dc14985840a2cd97d900f0812eb027792b6d81
unknown <pdelkowski@KRZ25092014L088.integer.loc>
2014-10-01 16:46:25 +0200

    end of lesson 3, generated CRUD for jobs

--- web/fronted_dev.php
+++ web/fronted_dev.php
@@ -7,6 +7,8 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
+error_reporting(E_ALL & ~E_DEPRECATED);
+
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('fronted', 'dev', true);
